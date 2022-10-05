ccf8eed9db24d6aa08a6923ee63d3f445e0c1bb3
Julio Lemos <julio.santana@justa.com.vc>
2022-10-04 22:48:18 -0300

    refactor: change HttpPostClient to receive an object

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,7 +1,11 @@
+export type HttpPostParams = {
+  url: string
+}
+
 /**
  * Abstração de Produção responsável pelo Post do HttpClient
  * Respeita o padrão do Interface Segregation Principle
  */
 export interface HttpPostClient {
-  post: (url: string) => Promise<void>
+  post: (params: HttpPostParams) => Promise<void>
 }
