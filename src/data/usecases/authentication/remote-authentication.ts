8ed67e822b42639cee7123acd64329b75f43ace5
Julio Lemos <julio.santana@justa.com.vc>
2022-10-05 01:16:58 -0300

    feat: ensure RemoteAuthentication return an Account on success

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -3,23 +3,23 @@ import { HttpStatusCode } from '~/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '~/domain/errors/invalid-credentials-error'
 import { UnexpectedError } from '~/domain/errors/unexpected-error'
 import { AccountModel } from '~/domain/models/account-model'
-import { AuthenticationParams } from '~/domain/usecases/authentication'
+import { Authentication, AuthenticationParams } from '~/domain/usecases/authentication'
 
 /** Classe de produção responsável por se conectar a API de Authenticação  */
-export class RemoteAuthentication {
+export class RemoteAuthentication implements Authentication {
   constructor (
     private readonly url: string,
     private readonly httpClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpClient.post({ url: this.url, body: params })
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.unauthorized:
         throw new InvalidCredentialsError()
       case HttpStatusCode.ok:
-        break
+        return httpResponse.body as AccountModel
       default:
         throw new UnexpectedError()
     }
